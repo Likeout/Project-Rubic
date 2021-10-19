8873c1c48b7e827072a16be754d7a535b889d80b
AcruxTech <83660115+Likeout@users.noreply.github.com>
2021-10-19 15:52:12 +0300

    Сделал сервак.

---
+++ ProjectRubic/ProjectRubic/Contexts/NoteContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectRubic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectRubic.Contexts
+{
+    public class NoteContext: DbContext
+    {
+        public NoteContext(DbContextOptions<NoteContext> options): base(options)
+        { }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
